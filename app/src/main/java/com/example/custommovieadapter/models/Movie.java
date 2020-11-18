6f33b4054b21edf09d258336e7514859cad7d734
falakdesai <falak.desai@yahoo.com>
2020-11-17 23:56:57 -0500

    added 4 private variables in class.

--- app/src/main/java/com/example/custommovieadapter/models/Movie.java
+++ app/src/main/java/com/example/custommovieadapter/models/Movie.java
@@ -1,4 +1,10 @@
 package com.example.custommovieadapter.models;
 
 public class Movie {
+
+    private String movieTitle;
+    private String movieRating;
+    private String movieDescription;
+    private int movieImage;
+
 }
