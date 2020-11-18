e938f73236054f567ea4282e31157b33f24699da
falakdesai <falak.desai@yahoo.com>
2020-11-18 00:22:48 -0500

    edited getCount, getItem and getItemId methods

--- app/src/main/java/com/example/custommovieadapter/adapters/MovieCustomAdapter.java
+++ app/src/main/java/com/example/custommovieadapter/adapters/MovieCustomAdapter.java
@@ -4,24 +4,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.example.custommovieadapter.models.Movie;
+
+import java.util.ArrayList;
+
 public class MovieCustomAdapter extends BaseAdapter {
+
+    ArrayList<Movie> movies;
+
+    public MovieCustomAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return movies.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Movie getItem(int position) {
+        return movies.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View view, ViewGroup viewGroup) {
         return null;
     }
 }
